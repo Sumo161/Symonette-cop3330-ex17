ecfc0ec525b12d00b30c1ffe9145c5ca88541a87
Sumo161 <84353899+Sumo161@users.noreply.github.com>
2021-06-06 16:06:15 -0400

    Update App.java

--- src/main/java/ex17/App.java
+++ src/main/java/ex17/App.java
@@ -1,5 +1,8 @@
 package ex17;
-
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Nyles Symonette
+ */
 import java.util.Scanner;
 public class App {
     public static void main(String[] args){
