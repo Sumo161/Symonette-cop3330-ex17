93c3902a1de9fe8d7be01808f81448aa727c1adf
sumo101 <symonette@knights.ucf.edu>
2021-06-06 16:04:17 -0400

    Initial solution to problem 17

---
+++ src/main/java/ex17/App.java
@@ -0,0 +1,49 @@
+package ex17;
+
+import java.util.Scanner;
+public class App {
+    public static void main(String[] args){
+
+        // stating the struct and introducing variables
+        int a, w, h, g;
+        float r;
+        Double BAC;
+        Scanner sc = new Scanner(System.in);
+
+
+        // concat
+        System.out.print("Enter a 1 is you are male or a 2 if you are female:");
+        g = sc.nextInt();
+
+        System.out.print("How many ounces of alchol did you have? ");
+        a = sc.nextInt();
+
+        System.out.print("What is your weight, in pounds? ");
+        w = sc.nextInt();
+
+        System.out.print("How many hours has it been since your last drink? ");
+        h = sc.nextInt();
+
+        sc.close();
+
+        //establish if/else statement ot determine output to user
+        if ( g==1 ){
+            r = (float) 0.73;
+        }
+        else{
+            r = (float) 0.66;
+        }
+        BAC = (a * 5.14/ w * r) - (0.015 * h);
+
+        System.out.println("Your BAC is " +BAC);
+        if(BAC<0.08){
+            System.out.println("It's legal for you to drive. ");
+        }
+        else{
+            System.out.println("It's illegal to drive. ");
+        }
+
+
+
+    }
+}
